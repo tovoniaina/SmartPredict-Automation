f92d1b9e0985ee6a72e1a4a40824a18039c1d38c
Aristide <idealy371@gmail.com>
2022-01-12 15:43:38 +0300

    reinstallation repos cypress

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,6 +18,26 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
 
 // Cypress.Server.defaults({
 //     delay: 500,
@@ -38,4 +58,3 @@ if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
 
     app.document.head.appendChild(style);
 }
-
